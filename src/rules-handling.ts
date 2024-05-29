1a15474c5bf434c423010e7e36e17c2edd607278
Duncan Cross <duncan.cross@gmail.com>
2024-05-29 21:15:42 +0200

    WIP rules handling

---
+++ src/rules-handling.ts
@@ -0,0 +1,200 @@
+
+const identEscapePattern = /\\(?:[^\r\n\f0-9a-fA-F]|[0-9a-fA-F]{1,6}(?:[ \t\n\f]|\r\n?)?)/ug;
+
+const stringEscapePattern = new RegExp(
+  identEscapePattern.source.replace(/\)$/, '|' + /[\n\f]|\r\n?/.source + ')'),
+  'ug'
+);
+
+const identHeadCharPattern = new RegExp(
+  '(?:' + /[a-zA-Z_\u0080-\uFFFF]/.source + '|' + identEscapePattern.source + ')'
+);
+
+const identTailCharPattern = new RegExp(
+  '(?:' + /[a-zA-Z0-9_\-\u0080-\uFFFF]/.source + '|' + identEscapePattern.source + ')'
+);
+
+const identPattern = new RegExp(
+  '(?:--|-?' + identHeadCharPattern.source + ')' + identTailCharPattern.source + '*'
+);
+
+const MIDX_URL = 1;
+const MIDX_IDENT_CALL = 2;
+const MIDX_NUMBER_UNIT = 3;
+
+const tokenPattern = new RegExp([
+  /\/\*(?:[^]*?\*\/)?/.source,
+  /[ \t\r\n\f]+/.source,
+  // match[1]: url() token
+  /[uU][rR][lL]\([ \t\r\n\f]*([^"'()\\ \t\r\n\f\x00-\x08\x0E-\x1F\x7F]+)[ \t\r\n\f]*\)/.source,
+  // match[2]: ident-is-call token
+  identPattern.source + '([(])?',
+  '@' + identPattern.source,
+  '#' + identTailCharPattern.source + '+',
+  '"(?:' + /[^"\\\n\r\f]/.source + '+|' + stringEscapePattern.source + ')*"',
+  "'(?:" + /[^'\\\n\r\f]/.source + '+|' + stringEscapePattern.source + ")*'",
+  // match[3]: number-unit token
+  /[+\-]?(?:\.[0-9]+|[0-9]+(?:\.[0-9]+)?)(?:[eE][+\-]?[0-9]+)?/.source + '(%|' + identPattern.source + ')?',
+  /[^]/.source, // any single character not already matched
+].join('|'), 'gyu');
+
+type ContentToken = {
+  type: 'comment' | 'whitespace' | 'string' | 'identifier' | 'at-identifier' | 'hash' | 'symbol' | 'call-open' | 'url';
+  content: string;
+};
+
+type NumericToken = {
+  type: 'number';
+  value: number;
+  unit?: string;
+};
+
+export type FlatToken = ContentToken | NumericToken;
+
+const escapeReplace = (str: string) => {
+  const hexMatch = str.match(/^\\([0-9a-fA-F]+)/);
+  if (hexMatch) {
+    const hex = parseInt(hexMatch[1], 16);
+    if (hex > 0x10ffff) {
+      throw new SyntaxError('Unicode escape out of range');
+    }
+    return String.fromCodePoint(hex);
+  }
+  return str.slice(1);
+};
+
+export function *eachToken(text: string): Generator<FlatToken> {
+  const rx = new RegExp(tokenPattern);
+  for (let m = rx.exec(text); m; m = rx.exec(text)) {
+    switch (m[0][0]) {
+      case ' ': case '\t': case '\r': case '\n': case '\f': {
+        yield {type:'whitespace', content:m[0]};
+        continue;
+      }
+      case '/': {
+        if (m[0][1] === '*') {
+          if (m.length === 2) {
+            throw new SyntaxError('unterminated comment');
+          }
+          yield {type:'comment', content:m[0].slice(2, -2)};
+        }
+        else {
+          yield {type:'symbol', content:m[0]};
+        }
+        continue;
+      }
+      case '"': case "'": {
+        if (m[0].length === 1) {
+          throw new SyntaxError('unterminated string');
+        }
+        const content = m[0].slice(1, -1).replace(stringEscapePattern, escapeReplace);
+        yield {type:'string', content};
+        continue;
+      }
+      case '#': {
+        if (m[0].length === 1) {
+          yield {type:'symbol', content:'#'};
+        }
+        else {
+          yield {type:'hash', content:m[0].slice(1).replace(identEscapePattern, escapeReplace)};
+        }
+        continue;
+      }
+      case '@': {
+        if (m[0].length === 1) {
+          yield {type:'symbol', content:'@'};
+        }
+        else {
+          yield {type:'at-identifier', content:m[0].slice(1).replace(identEscapePattern, escapeReplace)};
+        }
+        continue;
+      }
+      case '-': {
+        if (m[0].length === 1) {
+          yield {type:'symbol', content:'-'};
+        }
+        else if (m[0][1] === '.' || (m[0][1] >= '0' && m[0][1] <= '9')) {
+          const unit = m[MIDX_NUMBER_UNIT];
+          if (unit) {
+            yield {
+              type: 'number',
+              value: Number(m[0].slice(0, -unit.length)),
+              unit: unit.replace(identEscapePattern, escapeReplace),
+            };
+          }
+          else {
+            yield {type:'number', value:Number(m[0])};
+          }
+        }
+        else {
+          if (m[MIDX_IDENT_CALL]) {
+            yield {type:'call-open', content:m[0].slice(0, -1).replace(identEscapePattern, escapeReplace)};
+          }
+          else {
+            yield {type:'identifier', content:m[0].replace(identEscapePattern, escapeReplace)};
+          }
+        }
+        continue;
+      }
+      case '+': {
+        if (m[0].length === 1) {
+          yield {type:'symbol', content:'+'};
+        }
+        else {
+          const unit = m[MIDX_NUMBER_UNIT];
+          if (unit) {
+            yield {
+              type: 'number',
+              value: Number(m[0].slice(0, -unit.length)),
+              unit: unit.replace(identEscapePattern, escapeReplace),
+            };
+          }
+          else {
+            yield {type:'number', value:Number(m[0])};
+          }
+        }
+        continue;
+      }
+      case '.':
+      case '0': case '1': case '2': case '3': case '4':
+      case '5': case '6': case '7': case '8': case '9': {
+        if (m[0] === '.') {
+          yield {type:'symbol', content:'.'};
+        }
+        else {
+          const unit = m[MIDX_NUMBER_UNIT];
+          if (unit) {
+            yield {
+              type: 'number',
+              value: Number(m[0].slice(0, -unit.length)),
+              unit: unit.replace(identEscapePattern, escapeReplace),
+            };
+          }
+          else {
+            yield {type:'number', value:Number(m[0])};
+          }
+        }
+        continue;
+      }
+      case '\\': {
+        if (m.length === 1) {
+          throw new SyntaxError('invalid escape');
+        }
+        break;
+      }
+    }
+    if (/^[!#$%&()*+,\-\.\/:;<=>?@\[\]\^`{|}~]$/.test(m[0])) {
+      yield {type:'symbol', content:m[0]};
+    }
+    else if (m[MIDX_URL]) {
+      yield {type:'url', content:m[MIDX_URL]};
+    }
+    else if (m[MIDX_IDENT_CALL]) {
+      yield {type:'call-open', content:m[0].slice(0,-1).replace(identEscapePattern, escapeReplace)};
+    }
+    else {
+      console.log(JSON.stringify(m[0]));
+      yield {type:'identifier', content:m[0].replace(identEscapePattern, escapeReplace)};
+    }
+  }
+}
---
+++ tests/rules-tests.ts
@@ -0,0 +1,280 @@
+
+import { test, describe, it } from 'node:test';
+import assert from 'node:assert/strict';
+import { eachToken, FlatToken } from '../src/rules-handling';
+
+test('rules tokenization', async (ctx) => {
+
+  const assertEqualTokens = (text: string, tokens: FlatToken[]) => {
+    assert.deepEqual([...eachToken(text)], tokens);
+  }
+
+  await ctx.test('whitespace', (ctx) => {
+    assertEqualTokens(' \r\n \t \f \r \n ', [
+      {type:'whitespace', content:' \r\n \t \f \r \n '}
+    ])
+  });
+
+  await ctx.test('comments', async (ctx) => {
+    await ctx.test('simple comment', async (ctx) => {
+      assertEqualTokens('/* bla bla */', [
+        {type:'comment', content:' bla bla '},
+      ]);
+    });
+
+    await ctx.test('comment nesting (lack thereof)', async (ctx) => {
+      assertEqualTokens('/* nested /* comments are */ not supported */', [
+        {type: 'comment', content: ' nested /* comments are '},
+        {type: 'whitespace', content: ' '},
+        {type: 'identifier', content: 'not'},
+        {type: 'whitespace', content: ' '},
+        {type: 'identifier', content: 'supported'},
+        {type: 'whitespace', content: ' '},
+        {type: 'symbol', content:'*'},
+        {type: 'symbol', content:'/'},
+      ]);
+    });
+    
+  });
+
+  await ctx.test('identifiers', async ctx => {
+
+    await ctx.test('simple identifiers', async ctx => {
+      assertEqualTokens('an_identifier-123e9', [
+        {type: 'identifier', content:'an_identifier-123e9'},
+      ]);
+
+      assertEqualTokens('-single-dash-identifier', [
+        {type: 'identifier', content:'-single-dash-identifier'},
+      ]);
+
+      assertEqualTokens('--1-double-dash-identifier', [
+        {type: 'identifier', content:'--1-double-dash-identifier'},
+      ]);
+    });
+
+    await ctx.test('identifiers with escapes', async (ctx) => {
+      assertEqualTokens(' \\  ', [
+        {type: 'whitespace', content: ' '},
+        {type: 'identifier', content: ' '},
+        {type: 'whitespace', content: ' '},
+      ]);
+
+      assertEqualTokens('\\4F \\00004B3', [
+        {type: 'identifier', content: 'OK3'},
+      ]);
+    });
+
+  });
+
+  await ctx.test('calls', async ctx => {
+
+    await ctx.test('simple calls', async ctx => {
+      assertEqualTokens('an_identifier-123e9()', [
+        {type: 'call-open', content:'an_identifier-123e9'},
+        {type: 'symbol', content:')'},
+      ]);
+
+      assertEqualTokens('-single-dash-identifier()', [
+        {type: 'call-open', content:'-single-dash-identifier'},
+        {type: 'symbol', content:')'},
+      ]);
+
+      assertEqualTokens('--1-double-dash-identifier()', [
+        {type: 'call-open', content:'--1-double-dash-identifier'},
+        {type: 'symbol', content:')'},
+      ]);
+    });
+
+    await ctx.test('identifiers with escapes', async (ctx) => {
+      assertEqualTokens(' \\ () ', [
+        {type: 'whitespace', content: ' '},
+        {type: 'call-open', content: ' '},
+        {type: 'symbol', content:')'},
+        {type: 'whitespace', content: ' '},
+      ]);
+
+      assertEqualTokens('\\4F \\00004B3', [
+        {type: 'call-open', content: 'OK3'},
+        {type: 'symbol', content:')'},
+      ]);
+    });
+
+  });
+
+  await ctx.test('at-identifiers', async ctx => {
+
+    await ctx.test('simple identifiers', async ctx => {
+      assertEqualTokens('@an_identifier-123e9', [
+        {type: 'at-identifier', content:'an_identifier-123e9'},
+      ]);
+
+      assertEqualTokens('@-single-dash-identifier', [
+        {type: 'at-identifier', content:'-single-dash-identifier'},
+      ]);
+
+      assertEqualTokens('@--1-double-dash-identifier', [
+        {type: 'at-identifier', content:'--1-double-dash-identifier'},
+      ]);
+    });
+
+    await ctx.test('identifiers with escapes', async (ctx) => {
+      assertEqualTokens(' @\\  ', [
+        {type: 'whitespace', content: ' '},
+        {type: 'at-identifier', content: ' '},
+        {type: 'whitespace', content: ' '},
+      ]);
+
+      assertEqualTokens('@\\4F \\00004B3', [
+        {type: 'at-identifier', content: 'OK3'},
+      ]);
+    });
+
+  });
+
+  await ctx.test('hash', async ctx => {
+
+    await ctx.test('simple identifiers', async ctx => {
+      assertEqualTokens('#an_identifier-123e9', [
+        {type: 'hash', content:'an_identifier-123e9'},
+      ]);
+
+      assertEqualTokens('#1-digit-begins', [
+        {type: 'hash', content:'1-digit-begins'},
+      ]);
+    });
+
+    await ctx.test('identifiers with escapes', async (ctx) => {
+      assertEqualTokens(' #\\  ', [
+        {type: 'whitespace', content: ' '},
+        {type: 'hash', content: ' '},
+        {type: 'whitespace', content: ' '},
+      ]);
+
+      assertEqualTokens('#\\4F \\00004B3', [
+        {type: 'hash', content: 'OK3'},
+      ]);
+    });
+
+  });
+
+  await ctx.test('numbers', async ctx => {
+
+    await ctx.test('plain numbers', async ctx => {
+      assertEqualTokens('100', [
+        {type: 'number', value: 100},
+      ]);
+      assertEqualTokens('0.5', [
+        {type: 'number', value: 0.5},
+      ]);
+      assertEqualTokens('-.25', [
+        {type: 'number', value:-.25},
+      ])
+    });
+
+    await ctx.test('scientific notation', async ctx => {
+      assertEqualTokens('1e3', [
+        {type: 'number', value: 1e3},
+      ]);
+      assertEqualTokens('1E5', [
+        {type: 'number', value: 1E5},
+      ]);
+      assertEqualTokens('-1e+2', [
+        {type: 'number', value: -1e+2},
+      ]);
+      assertEqualTokens('1E-5', [
+        {type: 'number', value: 1E-5},
+      ]);
+    });
+
+    await ctx.test('dimensions and percentages', async ctx => {
+      assertEqualTokens('3px', [
+        {type: 'number', value: 3, unit: 'px'},
+      ]);
+      assertEqualTokens('-11%', [
+        {type: 'number', value: -11, unit: '%'},
+      ]);
+      assertEqualTokens('2.5e', [
+        {type: 'number', value: 2.5, unit: 'e'},
+      ]);
+      assertEqualTokens('45deg', [
+        {type: 'number', value: 45, unit: 'deg'},
+      ]);
+      assertEqualTokens('1e3e3', [
+        {type: 'number', value: 1e3, unit: 'e3'},
+      ]);
+    });
+
+  });
+
+  await ctx.test('strings', async ctx => {
+
+    await ctx.test('double quoted', async ctx => {
+      await ctx.test('simple string', async ctx => {
+        assertEqualTokens(`"blah"`, [
+          {type:'string', content:'blah'},
+        ]);
+      });
+      await ctx.test('escapes', async ctx => {
+        assertEqualTokens(String.raw `"\"\\\4F \00004B3${'\\\r\n'}"`, [
+          {type:'string', content: "\"\\OK3\r\n"},
+        ])
+      });
+      await ctx.test('unterminated (end of content)', async ctx => {
+        let error: unknown = null;
+        try {
+          for (const tok of eachToken('"')) { }
+        }
+        catch (e) {
+          error = e;
+        }
+        assert.ok(error instanceof SyntaxError, 'unterminated string did not produce syntax error');
+      });
+      await ctx.test('unterminated (unescaped newline)', async ctx => {
+        let error: unknown = null;
+        try {
+          for (const tok of eachToken('"string\r\n"')) { }
+        }
+        catch (e) {
+          error = e;
+        }
+        assert.ok(error instanceof SyntaxError, 'unterminated string did not produce syntax error');
+      });
+    });
+
+    await ctx.test('single quoted', async ctx => {
+      await ctx.test('simple string', async ctx => {
+        assertEqualTokens(`'blah'`, [
+          {type:'string', content:'blah'},
+        ]);
+      });
+      await ctx.test('escapes', async ctx => {
+        assertEqualTokens(String.raw `'\'\\\4F \00004B3${'\\\r\n'}'`, [
+          {type:'string', content: "'\\OK3\r\n"},
+        ])
+      });
+      await ctx.test('unterminated (end of content)', async ctx => {
+        let error: unknown = null;
+        try {
+          for (const tok of eachToken(`'`)) { }
+        }
+        catch (e) {
+          error = e;
+        }
+        assert.ok(error instanceof SyntaxError, 'unterminated string did not produce syntax error');
+      });
+      await ctx.test('unterminated (unescaped newline)', async ctx => {
+        let error: unknown = null;
+        try {
+          for (const tok of eachToken("'string\r\n'")) { }
+        }
+        catch (e) {
+          error = e;
+        }
+        assert.ok(error instanceof SyntaxError, 'unterminated string did not produce syntax error');
+      });
+    });
+
+  });
+
+});
